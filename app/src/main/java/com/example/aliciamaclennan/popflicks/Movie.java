ddca6afdf89c3b8a06c2fb58483b6f0f5125b0f4
Alicia MacLennan <aflyingfemme@gmail.com>
2016-08-27 19:15:39 -0700

    Build movie class

--- app/src/main/java/com/example/aliciamaclennan/popflicks/Movie.java
+++ app/src/main/java/com/example/aliciamaclennan/popflicks/Movie.java
@@ -4,4 +4,24 @@ package com.example.aliciamaclennan.popflicks;
  * Created by amac on 8/27/16.
  */
 public class Movie {
+
+    private String mTitle;
+
+    private String mDescription;
+
+    private String mRelease;
+
+
+    public Movie(String title, String description, String release) {
+        mTitle = title;
+        mDescription = description;
+        mRelease = release;
+    }
+
+    public String getTitle() { return mTitle; }
+    public String getDescription() {
+        return mDescription;
+    }
+    public String getRelease() { return mRelease; }
 }
+
