f1b77d196d8799f7cde56428a229c469c894bac7
Alicia MacLennan <aflyingfemme@gmail.com>
2016-08-27 19:15:14 -0700

    Add JSON request utility

--- app/src/main/java/com/example/aliciamaclennan/popflicks/QueryUtils.java
+++ app/src/main/java/com/example/aliciamaclennan/popflicks/QueryUtils.java
@@ -1,7 +1,194 @@
 package com.example.aliciamaclennan.popflicks;
 
+import android.media.Image;
+import android.text.TextUtils;
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by amac on 8/27/16.
  */
-public class QueryUtils {
+public final class QueryUtils {
+
+
+    private static final String LOG_TAG = QueryUtils.class.getSimpleName();
+
+    private QueryUtils() {
+    }
+
+    /**
+     * Query The Movie DB and return a list of {@link Movie} objects.
+     */
+    public static List<Movie> fetchMovieData(String requestUrl) {
+        // Create URL object
+        URL url = createUrl(requestUrl);
+
+        // Perform HTTP request to the URL and receive a JSON response back
+        String jsonResponse = null;
+        try {
+            jsonResponse = makeHttpRequest(url);
+        } catch (IOException e) {
+            Log.e(LOG_TAG, "Problem making the HTTP request.", e);
+        }
+
+        // Extract relevant fields from the JSON response and create a list of {@link Earthquake}s
+        List<Movie> movies = extractFeatureFromJson(jsonResponse);
+
+        // Return the list of {@link Earthquake}s
+        return movies;
+    }
+
+    /**
+     * Returns new URL object from the given string URL.
+     */
+    private static URL createUrl(String stringUrl) {
+        URL url = null;
+        try {
+            url = new URL(stringUrl);
+        } catch (MalformedURLException e) {
+            Log.e(LOG_TAG, "Problem building the URL ", e);
+        }
+        return url;
+    }
+
+    /**
+     * Make an HTTP request to the given URL and return a String as the response.
+     */
+    private static String makeHttpRequest(URL url) throws IOException {
+        String jsonResponse = "";
+
+        // If the URL is null, then return early.
+        if (url == null) {
+            return jsonResponse;
+        }
+
+        HttpURLConnection urlConnection = null;
+        InputStream inputStream = null;
+        try {
+            urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection.setReadTimeout(10000 /* milliseconds */);
+            urlConnection.setConnectTimeout(15000 /* milliseconds */);
+            urlConnection.setRequestMethod("GET");
+            urlConnection.connect();
+
+            // If the request was successful (response code 200),
+            // then read the input stream and parse the response.
+            if (urlConnection.getResponseCode() == 200) {
+                inputStream = urlConnection.getInputStream();
+                jsonResponse = readFromStream(inputStream);
+            } else {
+                Log.e(LOG_TAG, "Error response code: " + urlConnection.getResponseCode());
+            }
+        } catch (IOException e) {
+            Log.e(LOG_TAG, "Problem retrieving the movie JSON results.", e);
+        } finally {
+            if (urlConnection != null) {
+                urlConnection.disconnect();
+            }
+            if (inputStream != null) {
+                // Closing the input stream could throw an IOException, which is why
+                // the makeHttpRequest(URL url) method signature specifies than an IOException
+                // could be thrown.
+                inputStream.close();
+            }
+        }
+        return jsonResponse;
+    }
+
+    /**
+     * Convert the {@link InputStream} into a String which contains the
+     * whole JSON response from the server.
+     */
+    private static String readFromStream(InputStream inputStream) throws IOException {
+        StringBuilder output = new StringBuilder();
+        if (inputStream != null) {
+            InputStreamReader inputStreamReader = new InputStreamReader(inputStream, Charset.forName("UTF-8"));
+            BufferedReader reader = new BufferedReader(inputStreamReader);
+            String line = reader.readLine();
+            while (line != null) {
+                output.append(line);
+                line = reader.readLine();
+            }
+        }
+        return output.toString();
+    }
+
+
+    private static List<Movie> extractFeatureFromJson(String movieJSON) {
+        // If the JSON string is empty or null, then return early.
+        if (TextUtils.isEmpty(movieJSON)) {
+            return null;
+        }
+
+        // Create an empty ArrayList that we can start adding earthquakes to
+        List<Movie> movies = new ArrayList<>();
+
+        // Try to parse the JSON response string. If there's a problem with the way the JSON
+        // is formatted, a JSONException exception object will be thrown.
+        // Catch the exception so the app doesn't crash, and print the error message to the logs.
+        try {
+
+            // Create a JSONObject from the JSON response string
+            JSONObject baseJsonResponse = new JSONObject(movieJSON);
+
+            // Extract the JSONArray associated with the key called "results",
+            // which represents a list of movies.
+            JSONArray movieArray = baseJsonResponse.getJSONArray("results");
+
+            // For each earthquake in the movieArray, create an {@link movie} object
+            for (int i = 0; i < movieArray.length(); i++) {
+
+                // Get a single movie at position i within the list of earthquakes
+                JSONObject currentMovie = movieArray.getJSONObject(i);
+
+                // For a given movie, extract the JSONObject associated with the
+                // key called "properties", which represents a list of all properties
+                // for that movie.
+                String title = currentMovie.getString("title");
+
+                // Extract the value for the key called "mag"
+                String release = currentMovie.getString("release_date");
+
+                // Extract the value for the key called "place"
+                String description = currentMovie.getString("overview");
+
+                // Extract the value for the key called "time"
+                String poster = currentMovie.getString("poster_path");
+
+                // This is to get the movie poster, but will be a separate call
+                String url = "https://image.tmdb.org/t/p/w600_and_h900_bestv2" + poster;
+
+                // Create a new {@link Earthquake} object with the magnitude, location, time,
+                // and url from the JSON response.
+                Movie movie = new Movie(title, description, release);
+
+                // Add the new {@link Earthquake} to the list of earthquakes.
+                movies.add(movie);
+            }
+
+        } catch (JSONException e) {
+            // If an error is thrown when executing any of the above statements in the "try" block,
+            // catch the exception here, so the app doesn't crash. Print a log message
+            // with the message from the exception.
+            Log.e("QueryUtils", "Problem parsing the earthquake JSON results", e);
+        }
+
+        // Return the list of earthquakes
+        return movies;
+    }
+
 }
