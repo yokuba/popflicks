9ecfa65a91418803c6a980501bbd45bfe0737ff6
Alicia MacLennan <aflyingfemme@gmail.com>
2016-08-28 20:27:54 -0700

    Update 'no movies' string

--- app/src/main/java/com/example/aliciamaclennan/popflicks/MovieActivity.java
+++ app/src/main/java/com/example/aliciamaclennan/popflicks/MovieActivity.java
@@ -1,31 +1,158 @@
 package com.example.aliciamaclennan.popflicks;
 
-import android.app.ListActivity;
+import android.app.LoaderManager;
+import android.app.LoaderManager.LoaderCallbacks;
+import android.content.Context;
+import android.content.Loader;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ListView;
+import android.widget.TextView;
 
-public class MovieActivity extends ListActivity
+import java.util.AbstractCollection;
+import java.util.ArrayList;
+import java.util.List;
 
-    {
+public abstract class MovieActivity extends AppCompatActivity
+                implements LoaderCallbacks<List<Movie>> {
 
-        String[] itemname ={
-                "Fight Club",
-                "The Lobster",
-                "Gravity",
-                "2001: Space Odyssey",
-                "The Apartment",
-                "Alien",
-                "The King and I",
-                "The Bridge"
-        };
+private static final String LOG_TAG = MovieActivity.class.getName();
 
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_main);
 
-            this.setListAdapter(new ArrayAdapter<String>(
-                    this, R.layout.movie_list,
-                    R.id.empty_view,itemname));
+private static final String MOVIE_REQUEST_URL =
+    "https://api.themoviedb.org/3/movie/popular?api_key=a1a85e7c222c06c7ce7595fc8d963767&append_to_release=videos"
+
+private static final int MOVIE_LOADER_ID = 1;
+
+private MovieAdapter mAdapter;
+
+private TextView mEmptyStateTextView;
+    private List<Movie> movies;
+
+    @Override
+protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Find a reference to the {@link ListView} in the layout
+        ListView movieListView = (ListView) findViewById(R.id.movie_list);
+
+        mEmptyStateTextView = (TextView) findViewById(R.id.empty_view);
+        movieListView.setEmptyView(mEmptyStateTextView);
+
+        // Create a new adapter that takes an empty list of movies as input
+        mAdapter = new MovieAdapter(this, new ArrayList<Movie>());
+
+        // Set the adapter on the {@link ListView}
+        // so the list can be populated in the user interface
+        movieListView.setAdapter(mAdapter);
+
+
+        // Get a reference to the ConnectivityManager to check state of network connectivity
+        ConnectivityManager connMgr = (ConnectivityManager)
+        getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        // Get details on the currently active default data network
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+
+        // If there is a network connection, fetch data
+        if (networkInfo != null && networkInfo.isConnected()) {
+        // Get a reference to the LoaderManager, in order to interact with loaders.
+        LoaderManager loaderManager = getLoaderManager();
+
+        // Initialize the loader. Pass in the int ID constant defined above and pass in null for
+        // the bundle. Pass in this activity for the LoaderCallbacks parameter (which is valid
+        // because this activity implements the LoaderCallbacks interface).
+        loaderManager.initLoader(MOVIE_LOADER_ID, null, this);
+        } else {
+        // Otherwise, display error
+        // First, hide loading indicator so error message will be visible
+        View loadingIndicator = findViewById(R.id.loading_indicator);
+        loadingIndicator.setVisibility(View.GONE);
+
+        // Update empty state with no connection error message
+        mEmptyStateTextView.setText(R.string.no_internet_connection);
         }
-}
+        }
+
+@Override
+public Loader<List<Movie>> onCreateLoader(int i, Bundle bundle) {
+        // Create a new loader for the given URL
+        return new MovieLoader(this, MOVIE_REQUEST_URL);
+        }
+
+    @Override
+    public void onLoadFinished(android.content.Loader<List<Movie>> loader, List<Movie> data) {
+        View loadingIndicator = findViewById(R.id.loading_indicator);
+        loadingIndicator.setVisibility(View.GONE);
+
+        // Set empty state text to display "No earthquakes found."
+        mEmptyStateTextView.setText(R.string.no_movies);
+
+        // Clear the adapter of previous earthquake data
+        mAdapter.clear();
+
+        // If there is a valid list of {@link Earthquake}s, then add them to the adapter's
+        // data set. This will trigger the ListView to update.
+        if (movies != null && !movies.isEmpty()) {
+            mAdapter.addAll(movies);
+        }
+
+    }
+
+
+
+
+
+
+@Override
+public void onLoaderReset(Loader<Object> loader) {
+        // Loader reset, so we can clear out our existing data.
+        mAdapter.clear();
+        }
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//    {
+//
+//        String[] itemname ={
+//                "Fight Club",
+//                "The Lobster",
+//                "Gravity",
+//                "2001: Space Odyssey",
+//                "The Apartment",
+//                "Alien",
+//                "The King and I",
+//                "The Bridge"
+//        };
+//
+//        @Override
+//        protected void onCreate(Bundle savedInstanceState) {
+//            super.onCreate(savedInstanceState);
+//            setContentView(R.layout.activity_main);
+//
+//            this.setListAdapter(new ArrayAdapter<String>(
+//                    this, R.layout.movie_list,
+//                    R.id.empty_view,itemname));
+//        }
+//}
