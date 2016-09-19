17193ab78ffa10f1d7d0cf00666238dab0503efa
Alicia MacLennan <aflyingfemme@gmail.com>
2016-09-19 08:38:29 -0700

    Comment out fragment class to rework fragment

--- app/src/main/java/com/example/aliciamaclennan/popflicks/MovieFragment.java
+++ app/src/main/java/com/example/aliciamaclennan/popflicks/MovieFragment.java
@@ -1,31 +1,31 @@
-package com.example.aliciamaclennan.popflicks;
-
-import android.database.Cursor;
-import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.LoaderManager;
-import android.support.v4.content.Loader;
-
-import java.util.List;
-
-/**
- * Created by amac on 9/9/16.
- */
-public class MovieFragment extends Fragment implements LoaderManager.LoaderCallbacks<Cursor> {
-
-        @Override
-        public Loader<List<Movie>> onCreateLoader(int position, Bundle bundle) {
-            // Create a new loader for the given URL
-            return new MovieLoader(this, MOVIE_REQUEST_URL + NOW_PLAYING);
-        }
-
-    @Override
-    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
-
-    }
-
-    @Override
-    public void onLoaderReset(Loader<Cursor> loader) {
-
-    }
-}
+//package com.example.aliciamaclennan.popflicks;
+//
+//import android.database.Cursor;
+//import android.os.Bundle;
+//import android.support.v4.app.Fragment;
+//import android.support.v4.app.LoaderManager;
+//import android.support.v4.content.Loader;
+//
+//import java.util.List;
+//
+///**
+// * Created by amac on 9/9/16.
+// */
+//public abstract class MovieFragment extends Fragment implements LoaderManager.LoaderCallbacks<Cursor> {
+//
+//        @Override
+//        public Loader<Cursor> onCreateLoader(int position, Bundle bundle) {
+//            // Create a new loader for the given URL
+//            return new MovieLoader(this, MOVIE_REQUEST_URL + NOW_PLAYING);
+//        }
+//
+//    @Override
+//    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+//
+//    }
+//
+//    @Override
+//    public void onLoaderReset(Loader<Cursor> loader) {
+//
+//    }
+//}
